6c97a7758933bc31b96856eac5533ebe017ab252
austinbhan <austinbhan@outlook.com>
2022-05-05 16:31:24 -0700

    Working Health Bar Numbers

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 // import functions and grab DOM elements
+import { randomHealth } from './utils.js';
+
+const goblinName = document.getElementById('goblin-name');
+const goblinNameButton = document.getElementById('goblin-name-button');
+const activeGoblinsDiv = document.getElementById('active-goblins-div');
 
 // let state
-
+let goblinNames = [];
 // set event listeners 
+
+goblinNameButton.addEventListener('click', () => {
+    let name = `${goblinName.value}: ${randomHealth()} health`;
+    goblinNames.push(name);
+    displayGoblins();
+});
+
+function displayGoblins() {
+    activeGoblinsDiv.textContent = '';
+    for (let name of goblinNames) {
+        const li = document.createElement('li');
+        li.textContent = name;
+        activeGoblinsDiv.append(li);
+    }
+}
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ utils.js
@@ -0,0 +1,4 @@
+export const randomHealth = function randomHealth() {
+    let health = Math.ceil(Math.random() * 5);
+    return health;
+}
