0924683df61b4e138644d326d7f83c8a3bce4570
austinbhan <austinbhan@outlook.com>
2022-05-06 12:21:43 -0700

    Working Goblin List

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 // import functions and grab DOM elements
 
-const goblinName = document.getElementById('goblin-name');
+
 const activeGoblinsDiv = document.getElementById('active-goblins-div');
 const goblinCreation = document.getElementById('goblin-creation');
 
 // let state
-let goblins = [];
+let goblins = [{ name: 'Broccoli', health: 5 }, { name: 'Carrot', health: 4 }];
 
 // set event listeners 
 
 // Goblin Name Button and Append Text
+displayGoblins();
 
 goblinCreation.addEventListener('submit', (e) => {
     e.preventDefault();
-    const newName = goblinName.value || 'Broccolini';
-    let newGoblin = { name: newName, health: (Math.ceil(Math.random() * 5)) };
-    goblins.push(`Goblin Name: ${newGoblin.name}, Goblin Health: ${newGoblin.health}`);
-    
+    const formD = new FormData(goblinCreation);
+    const goblinName = formD.get('goblin-name');    
+    let goblinStats = { name: goblinName, health: (Math.ceil(Math.random() * 5)) };
+    goblins.push(goblinStats);
+    // Test Console Log 
+    console.log(goblins);
     displayGoblins();
 });
 
@@ -24,11 +27,7 @@ function displayGoblins() {
     activeGoblinsDiv.textContent = '';
     for (let goblin of goblins) {
         let button = document.createElement('button');
-        button.textContent = goblin;
+        button.textContent = `${goblin.name}, ${goblin.health}`;
         activeGoblinsDiv.append(button);
-
-        button.addEventListener('click', () => {
-            // Insert More Code Here
-        });
     }
 }
