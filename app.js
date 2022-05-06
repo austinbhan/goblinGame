c6a7c309c8a8e5a3a67c69f4a360da30c81a7b86
austinbhan <austinbhan@outlook.com>
2022-05-06 13:02:59 -0700

    Created player health values, need to make gameplay function

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 
 const activeGoblinsDiv = document.getElementById('active-goblins-div');
 const goblinCreation = document.getElementById('goblin-creation');
+const startingHealth = document.getElementById('starting-health');
 
 // let state
 let goblins = [{ name: 'Broccoli', health: 5 }, { name: 'Carrot', health: 4 }];
+let playerHealth = 10;
 
 // set event listeners 
 
@@ -18,8 +20,6 @@ goblinCreation.addEventListener('submit', (e) => {
     const goblinName = formD.get('goblin-name');    
     let goblinStats = { name: goblinName, health: (Math.ceil(Math.random() * 5)) };
     goblins.push(goblinStats);
-    // Test Console Log 
-    console.log(goblins);
     displayGoblins();
 });
 
@@ -27,7 +27,16 @@ function displayGoblins() {
     activeGoblinsDiv.textContent = '';
     for (let goblin of goblins) {
         let button = document.createElement('button');
-        button.textContent = `${goblin.name}, ${goblin.health}`;
+        button.textContent = `${goblin.name}, ${goblin.health} health`;
         activeGoblinsDiv.append(button);
+
+        button.addEventListener('click', () => {
+            console.log('This button works!');
+            // Making Gameplay Function
+        })
     }
+}
+
+function gamePlay() {
+
 }
