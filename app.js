1cfe0ec1ebcc2642b12b06251f7fc330a21f8029
austinbhan <austinbhan@outlook.com>
2022-05-05 17:03:29 -0700

    Redoing Name Button, Rewrote Health RNG

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 // import functions and grab DOM elements
-import { randomHealth } from './utils.js';
 
 const goblinName = document.getElementById('goblin-name');
 const goblinNameButton = document.getElementById('goblin-name-button');
 const activeGoblinsDiv = document.getElementById('active-goblins-div');
 
-const startingHealth = document.getElementById('starting-health');
 
 // let state
-let goblinNames = [];
+let goblins = [{ name: 'Broccoli', health: `${randomHealth}` }];
+let goblinHealth = Math.ceil(Math.random() * 5);
+console.log(goblinHealth);
 // set event listeners 
 
 // Goblin Name Button and Append Text
 goblinNameButton.addEventListener('click', () => {
-    let name = `${goblinName.value}: ${randomHealth()} health`;
-    goblinNames.push(name);
+    let goblinStat = {
+        name: goblinName.value,
+        health: randomHealth,
+    };
+    goblins.push(goblinStat);
     displayGoblins();
 });
-function displayGoblins() {
+`function displayGoblins() {
     activeGoblinsDiv.textContent = '';
-    for (let name of goblinNames) {
+    for (let name of goblins) {
         const li = document.createElement('li');
         li.textContent = name;
         activeGoblinsDiv.append(li);
     }
-}
+}`
 
 // Set Player Health
 
--- utils.js
+++
@@ -1,4 +0,0 @@
-export function randomHealth() {
-    let health = Math.ceil(Math.random() * 5);
-    return health;
-};
